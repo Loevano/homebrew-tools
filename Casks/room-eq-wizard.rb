b06cfb63282025fa3cb59ff91cee7d3ff10b71e1
Jens <jens.loeven@gmail.com>
2026-07-11 10:50:28 +0200

    Add Room EQ Wizard cask

---
+++ Casks/room-eq-wizard.rb
@@ -0,0 +1,11 @@
+cask "room-eq-wizard" do
+  version "5.31.3"
+  sha256 "8f1c6b9ecae27b2f60ff3dfc803920607e42f312342d9d5472c2e38b55b699f5"
+
+  url "https://www.roomeqwizard.com/installers/REW_macos_5_31_3.dmg"
+  name "Room EQ Wizard"
+  desc "Room acoustics measurement and analysis software"
+  homepage "https://www.roomeqwizard.com/"
+
+  app "REW.app"
+end
