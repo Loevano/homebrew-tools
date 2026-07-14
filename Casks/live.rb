fd94a772ab6f95f5dd3f00d5d85e4a1eb31a7c48
Jens <jens.loeven@gmail.com>
2026-07-14 21:06:46 +0200

    updated live

--- Casks/live.rb
+++ Casks/live.rb
@@ -7,23 +7,12 @@ cask "live" do
   desc "Live MacBook cask collection"
   homepage "https://github.com/Loevano/homebrew-tools"
 
-  livecheck do
-    skip "Meta cask"
-  end
-
   depends_on cask: "loevano/tools/ableton-live-suite"
-  depends_on cask: "loevano/tools/arturia-software-center"
-  depends_on cask: "loevano/tools/brave-browser"
-  depends_on cask: "loevano/tools/companion"
   depends_on cask: "loevano/tools/dante-controller"
   depends_on cask: "loevano/tools/dante-virtual-soundcard"
   depends_on cask: "loevano/tools/ilok-license-manager"
-  depends_on cask: "loevano/tools/max8"
-  depends_on cask: "loevano/tools/native-access"
-  depends_on cask: "loevano/tools/pigments"
   depends_on cask: "loevano/tools/rme-madiface-usb-driver"
-  depends_on cask: "loevano/tools/standardclip"
   depends_on cask: "loevano/tools/totalmix-fx"
   depends_on cask: "loevano/tools/touchosc"
-  depends_on cask: "loevano/tools/u-he-diva"
+  depends_on cask: "reaper"
 end
