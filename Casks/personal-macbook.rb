b3ca7a6dfc8bdf9d50d4b602aec84c109671a516
Jens Loeven <jens@MacBook-Pro.home>
2026-08-16 19:39:26 +0200

    Add Personal MacBook meta cask

---
+++ Casks/personal-macbook.rb
@@ -0,0 +1,20 @@
+cask "personal-macbook" do
+  version "1.0"
+  sha256 :no_check
+
+  url "file:///dev/null"
+  name "Personal MacBook"
+  desc "Apps for a personal MacBook"
+  homepage "https://github.com/Loevano/homebrew-tools"
+
+  livecheck do
+    skip "Meta cask"
+  end
+
+  depends_on cask: "visual-studio-code"
+  depends_on cask: "whatsapp"
+  depends_on cask: "loevano/tools/brave-browser"
+  depends_on cask: "loevano/tools/fabfilter"
+  depends_on cask: "loevano/tools/ableton-live-suite"
+  depends_on cask: "reaper"
+end
